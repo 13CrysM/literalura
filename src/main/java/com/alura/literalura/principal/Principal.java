d1e3d114c893b1738dc4f9fc8220d87e6d87c817
13CrysM <crysmupe@gamil.com>
2025-07-24 22:17:07 -0600

    Detallando impresión

--- src/main/java/com/alura/literalura/principal/Principal.java
+++ src/main/java/com/alura/literalura/principal/Principal.java
@@ -19,16 +19,16 @@ import java.util.stream.Collectors;
 @Component
 public class Principal {
 
-    private Scanner teclado = new Scanner(System.in);
-    private ConsumoAPI consumoAPI = new ConsumoAPI();
-    private ConvierteDatos convierteDatos = new ConvierteDatos();
+    private final Scanner teclado = new Scanner(System.in);
+    private final ConsumoAPI consumoAPI = new ConsumoAPI();
+    private final ConvierteDatos convierteDatos = new ConvierteDatos();
     private final String URL_BASE = "https://gutendex.com/books/";
 
 
-    private LibroService libroService;
-    private AutorService autorService;
-    private LibroRepository libroRepository;
-    private AutorRepository autorRepository;
+    private final LibroService libroService;
+    private final AutorService autorService;
+    private final LibroRepository libroRepository;
+    private final AutorRepository autorRepository;
 
     public Principal(LibroService libroService, AutorService autorService,
                      LibroRepository libroRepository, AutorRepository autorRepository) {
@@ -42,11 +42,14 @@ public class Principal {
         int opcion = -1;
         while (opcion != 0) {
             var menu = """
+                    
+                    *-*-*-* Menú de Opciones *-*-*-*
                     1 - Buscar libro
                     2 - Mostrar libros guardados
                     3 - Mostrar autores por nombre
                     4 - Mostrar autores vivos por años
-                    5 - Mostrar libros por idioma  
+                    5 - Mostrar libros por idioma
+                    ----------------------------------
                     0 - Salir
                     """;
                 System.out.println(menu);
@@ -59,10 +62,10 @@ public class Principal {
                 case 4 -> mostrarAutoresVivos();
                 case 5 -> mostrarLibrosLen();
                 case 0 -> {
-                    System.out.println("Cerrando la aplicación...");
+                    System.out.println("\n\t\tCerrando la aplicación...");
                     System.exit(0); // Finaliza completamente el programa
                 }
-                default -> System.out.println("Opción inválida");
+                default -> System.out.println("\n\t\tOpción inválida");
             }
         }
         teclado.close();
@@ -70,7 +73,7 @@ public class Principal {
 
     public void buscarLibroWeb(){
         //Realiza la consulta a la API
-        System.out.println("\nHola, ingresa el título a buscar: ");
+        System.out.println("\n\tHola, ingresa el título a buscar: ");
         var datoBuscado = teclado.nextLine();
         var json = consumoAPI.obtenerDatos(URL_BASE + "?search=" + datoBuscado.replace(" ", "+"));
 
@@ -78,47 +81,46 @@ public class Principal {
         List<LibroDTO> librosDTO = respuestaAPI.getResults();
 
         if (librosDTO == null || librosDTO.isEmpty()) {
-            System.out.println("No se encontraron coincidencias.");
+            System.out.println("\n\t\tNo se encontraron coincidencias.");
             return;
         }
-        //Muestra las considencias de la consulta
-        System.out.println("\n--- Coincidencias para: " + datoBuscado + " ---\n");
+        //Muestra las coincidencias de la consulta
+        System.out.println("\n\t\t--- Coincidencias para: " + datoBuscado + " ---\n");
         for (int i = 0; i < librosDTO.size(); i++) {
             LibroDTO libro = librosDTO.get(i);
-            System.out.println((i + 1) + ". " + libro.getTitulo() + " (" +
+            System.out.println("\t" + (i + 1) + ". " + libro.getTitulo() + " (" +
                     (libro.getAutores().isEmpty() ? "Autor desconocido" : libro.getAutores().get(0).getNombre()) + ")");
         }
         //Elección de libro para proceder al guardado en base de datos
-        System.out.print("\nIngresa el número del libro que deseas guardar (0 para cancelar): ");
+        System.out.print("\n\tIngresa el número del libro que deseas guardar (0 para cancelar): ");
         int opcion = leerOpcionSegura(0, librosDTO.size());
         //Opciones de guardado
         if (opcion == 0) {
-            System.out.println("Cancelado por el usuario.\nNinguún libro guardado.");
+            System.out.println("\n\t\tCancelado por el usuario.\nNingún libro guardado.");
             return;
         }
 
         if (opcion < 1 || opcion > librosDTO.size()) {
-            System.out.println("Número inválido. No se seleccionó ningún libro.");
+            System.out.println("\n\t\tNúmero inválido. No se seleccionó ningún libro.");
             return;
         }
 
         LibroDTO libroSeleccionado = librosDTO.get(opcion - 1);
-        System.out.println("\nLibro seleccionado:");
+        System.out.println("\n\tLibro seleccionado:");
         imprimirDetallesLibro(libroSeleccionado);
 
-        System.out.print("¿Deseas guardarlo en la base de datos? (s/n): ");
+        System.out.print("\n\t¿Deseas guardarlo en la base de datos? (s/n): ");
         String confirmar = teclado.nextLine();
         if (confirmar.equalsIgnoreCase("s")) {
-            guardarLibro(libroSeleccionado);  // Descomenta cuando tengas el método implementado
-            //System.out.println("✅ Libro guardado exitosamente.");
+            guardarLibro(libroSeleccionado);
         } else {
-            System.out.println("❌ Libro no guardado.");
+            System.out.println("\n\t\tLibro no guardado.");
         }
     }
 
     private void guardarLibro(LibroDTO libroDTO){
         if (libroService.existsByTitle(libroDTO.getTitulo())) {
-            System.out.println("\n❌ Libro '" + libroDTO.getTitulo() + "' ya está registrado en la base de datos.");
+            System.out.println("\n\t\tLibro '" + libroDTO.getTitulo() + "' ya está registrado en la base de datos.");
             return;
         }
 
@@ -131,11 +133,8 @@ public class Principal {
         Autor autor = libroDTO.getAutores().isEmpty() ? null : procesarAutor(libroDTO.getAutores().get(0), List.of(libroDTO));
         libro.setAutor(autor);
         // Si hay autor, agregamos el libro a su lista
-        if (autor != null) {
-            //autor.getLibros().add(libro);
-            System.out.println("");
-        } else {
-            System.out.println("⚠️ Advertencia: Libro sin información de autor.");
+        if (autor == null) {
+            System.out.println("\n\t\tAdvertencia: Libro sin información de autor.");
         }
     }
 
@@ -157,7 +156,7 @@ public class Principal {
             // Guardar los libros directamente si usas cascada en Autor no es necesario
             libroRepository.saveAll(nuevosLibros); // Asegúrate de tener libroRepository
 
-            System.out.println("Autor ya existente. Libros agregados a la base de datos.");
+            System.out.println("\n\t\tAutor ya existente. Libros agregados a la base de datos.");
             return autor;
         } else {
             Autor nuevoAutor = new Autor();
@@ -173,7 +172,7 @@ public class Principal {
                 return libro;
             }).collect(Collectors.toCollection(ArrayList::new));
             nuevoAutor.setLibros(libros);
-            System.out.println("Autor agregado a Base de datos.");
+            System.out.println("\n\t\tAutor agregado a Base de datos.");
             return autorRepository.save(nuevoAutor);
         }
     }
@@ -182,7 +181,7 @@ public class Principal {
         System.out.println("\n*************** Libros en Base de Datos ***************");
         List<Libro> listaLibros = libroService.findAll();
         if (listaLibros.isEmpty()){
-            System.out.println("\n\tNo hay libros Registrados.");
+            System.out.println("\n\t\tNo hay libros Registrados.");
         } else {
             imprimirLibrosBD(listaLibros);
         }
@@ -192,7 +191,7 @@ public class Principal {
         System.out.println("\n*************** Autores en Base de Datos ***************");
         List<Autor> listaAutores = autorService.getAllAutores();
         if (listaAutores.isEmpty()) {
-            System.out.println("No hay autores registrados.");
+            System.out.println("\n\t\tNo hay autores registrados.");
         } else {
             imprimirAutoresBD(listaAutores);
         }
@@ -202,42 +201,43 @@ public class Principal {
         System.out.println("\n\tIngresa el año: ");
         int año = validarNumero();
         List<Autor> autoresVivos =  autorRepository.findAutorVivo(año);
-        System.out.println("\n***** Autores vivos en el año: " + año + " *****");
+        System.out.println("\n\t***** Autores vivos en el año: " + año + " *****");
         imprimirAutoresBD(autoresVivos);
-        System.out.println("\n**********************************************");
+        System.out.println("\n\t**********************************************");
     }
 
     public void mostrarLibrosLen(){
         System.out.println("""
                 
-                Ingresa el idioma para realizar la busqueda: 
+                Ingresa el idioma para realizar la búsqueda:
                 es - Español
                 en - Ingles
                 fr - Frances
-                pt - Portugues
+                pt - Portugués
                 la - Latin
                 ru - Ruso
+                hu - Húngaro
                 """);
         var idiomaSelect = teclado.nextLine();
-        if (idiomaSelect.equals("es") || idiomaSelect.equals("en") || idiomaSelect.equals("pt") || idiomaSelect.equals("fr")| idiomaSelect.equals("la")| idiomaSelect.equals("ru") ){
+        if (idiomaSelect.equals("es") || idiomaSelect.equals("en") || idiomaSelect.equals("pt") || idiomaSelect.equals("fr") || idiomaSelect.equals("la") || idiomaSelect.equals("ru") || idiomaSelect.equals("hu")){
             List<Libro> librosIdioma = libroService.findByLanguage(idiomaSelect);
             if (!librosIdioma.isEmpty()){
-                System.out.println("Libros en idioma: " + idiomaSelect);
+                System.out.println("\n---------- Libros en idioma: " + idiomaSelect + "----------");
                 imprimirLibrosBD(librosIdioma);
             } else {
-                System.out.println("No existen libros registrados en ese idioma.");
+                System.out.println("\n\t\tNo existen libros registrados en ese idioma.");
             }
         } else {
-            System.out.println("Idioma no válido.");
+            System.out.println("\n\t\tIdioma no válido.");
         }
     }
 
     private int leerOpcionSegura(int min, int max) {
         while (true) {
             if (min == max) {
-                System.out.print("Presiona " + min + " para continuar: ");
+                System.out.print("\n\tPresiona " + min + " para continuar: ");
             } else {
-                System.out.print("Ingresa una opción (" + min + "-" + max + "): ");
+                System.out.print("\n\tIngresa una opción (" + min + "-" + max + "): ");
             }
 
             try {
@@ -245,44 +245,45 @@ public class Principal {
                 if (opcion >= min && opcion <= max) {
                     return opcion;
                 } else {
-                    System.out.println("⚠️ Opción fuera de rango. Intenta de nuevo.");
+                    System.out.println("\n\t\tOpción fuera de rango. Intenta de nuevo.");
                 }
             } catch (NumberFormatException e) {
-                System.out.println("⚠️ Entrada inválida. Por favor, ingresa un número válido.");
+                System.out.println("\n\t\tEntrada inválida. Por favor, ingresa un número válido.");
             }
         }
     }
 
     private void imprimirDetallesLibro(LibroDTO libro) {
-        System.out.println("-------------------------------------------");
-        System.out.println("📚 Título:     " + libro.getTitulo());
-        System.out.println("🌐 Idioma:     " + (libro.getIdiomas().isEmpty() ? "N/A" : libro.getIdiomas().get(0)));
-        System.out.println("⬇️ Descargas:  " + libro.getNumeroDescargas());
+        System.out.println("\n-------------------------------------------");
+        System.out.println("Título:     " + libro.getTitulo());
         if (libro.getAutores() != null && !libro.getAutores().isEmpty()) {
-            System.out.println("✍️ Autor(es):   " + libro.getAutores().stream()
+            System.out.println("Autor(es):   " + libro.getAutores().stream()
                     .map(AutorDTO::getNombre)
                     .collect(Collectors.joining(", ")));
         } else {
-            System.out.println("✍️ Autor(es):   No disponible");
+            System.out.println("Autor(es):   No disponible");
         }
+        System.out.println("Idioma:     " + (libro.getIdiomas().isEmpty() ? "N/A" : libro.getIdiomas().get(0)));
+        System.out.println("Descargas:  " + libro.getNumeroDescargas());
         System.out.println("-------------------------------------------");
     }
 
     private void imprimirLibrosBD(List<Libro> libros) {
             libros.forEach(libro -> {
-                System.out.println("-------------------------------------------------------");
+                System.out.println("\n-------------------------------------------------------");
                 System.out.println("Título:              " + libro.getTitulo());
                 System.out.println("Autor:               " + (libro.getAutor() != null ? libro.getAutor().getNombre() : "N/A"));
                 System.out.println("Idioma:              " + libro.getIdioma());
                 System.out.println("Número de descargas: " + libro.getDescargas());
             });
-        System.out.println("---Fin---");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("\n---------------------------Fin-------------------------");
 
     }
 
     private void imprimirAutoresBD(List<Autor> autores) {
         autores.forEach(autor -> {
-            System.out.println("-------------------------------------------------------");
+            System.out.println("\n-------------------------------------------------------");
             System.out.println("Nombre del Autor: " + autor.getNombre());
             System.out.println("Año de Nacimiento:" + autor.getAñoNacimiento());
             System.out.println("Año de Fallecimiento: " + autor.getAñoMuerte());
@@ -291,6 +292,8 @@ public class Principal {
                     .collect(Collectors.joining(", ", "[", "]"));
             System.out.println("Libros del Autor: " + titulos);
         });
+        System.out.println("-------------------------------------------------------");
+        System.out.println("\n---------------------------Fin-------------------------");
     }
 
     private int validarNumero() {
@@ -299,7 +302,7 @@ public class Principal {
             teclado.nextLine();
             return opcion;
         } catch (InputMismatchException e) {
-            System.out.println("❌ Entrada inválida. Se esperaba un número.");
+            System.out.println("\n\t\tEntrada inválida. Se esperaba un número.");
             teclado.nextLine();
             return -1;
         }
